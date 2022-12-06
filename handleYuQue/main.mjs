f4d077494d91dd251e4993506ff839895cb4e63e
zhangxin <862257810@qq.com.com>
2022-12-07 01:00:16 +0800

    finish yuque script

--- handleYuQue/main.mjs
+++ handleYuQue/main.mjs
@@ -1,11 +1,23 @@
 #! /usr/bin/env node
 import { fetch, $, question, fs, chalk } from "zx";
-// const cookie = await question("please enter cookie> ");
+
+class Logger {
+  info(text) {
+    console.log(chalk.green(`INFO : ${text}`));
+  }
+  error(text) {
+    console.log(chalk.red(`ERROR : ${text}`));
+  }
+}
+
+const logger = new Logger();
+
+// const cookie = await question(chalk.green("please enter cookie> "));
 
 const cookie =
-  "lang=zh-cn; yuque_ctoken=fe7JCO4WPYr-fokO_2SwH0iX; _yuque_session=Yxvt7FKhpiAjwHBHiuoSDAnTd7db5PHZpOgska-y5iI_9w1LaxXkVJLS70u2kPIP-YoXyjboffYRne10BrJJig==; current_theme=default; acw_tc=0b68a81616702064709506907e7158eef12b76c54d44f2b09c8b7780115c3e; lang=zh-cn";
+  "lang=zh-cn; yuque_ctoken=q8zsxQzZ8TeyF9KclihjvEDv; _yuque_session=kC4Wtyjde9lU30Bjjd_G1R1NWkCYN3bhEzotXj5F8vkQ-hQh0ctt0oF1_yaCKT1MjVuArMBulKHTUoLKPNluaQ==; current_theme=default; acw_tc=0bca322a16703448813033862e894d8c966c010c485e2149c0d00945c63d8b";
 
-const getNoteList = async () => {
+const getNoteMap = async () => {
   const response = await fetch(
     "https://www.yuque.com/api/docs?book_id=10878407",
     {
@@ -17,8 +29,7 @@ const getNoteList = async () => {
     }
   );
   const { data } = await response.json();
-  const noteList = data.map((note) => ({ id: note.id, title: note.title }));
-  console.log(noteList);
+  return data.map((note) => ({ id: note.id, title: note.title }));
 };
 
 const getExportedContentUrl = async (id) => {
@@ -36,38 +47,56 @@ const getExportedContentUrl = async (id) => {
       referer: "https://www.yuque.com/manmanmanman-jrzm5/wow1yv/di1oha",
     },
   });
-  const result = await response.json();
-  return result.data.url;
-};
-
-const getFileName = (str) => {
-  return str.split("=")[1];
+  if (response.ok) {
+    const result = await response.json();
+    return result.data.url;
+  }
+  return response.ok;
 };
 
 /**
  * 处理markdown中的图片格式
  * @param  string text
  */
-const parseText = (text) => {};
+const parseText = (text) => {
+  const regp = /#clientId=[^)]*/g;
+  return text.replaceAll(regp, "");
+};
 
-const writeFile = (fileName, content) => {};
+const writeFile = (fileName, content) => {
+  logger.info(`writing ${fileName}...`);
+  let directory = "./output";
+  if (!fs.existsSync(directory)) {
+    // 创建文件夹
+    fs.mkdirSync(directory);
+  }
+  fs.writeFileSync(`${directory}/${fileName}`, content);
+  logger.info(`writing ${fileName} done`);
+};
 
 const getExportedContent = async (url) => {
   const response = await fetch(url, {
     method: "GET",
     headers: {
-      cookie:
-        "lang=zh-cn; yuque_ctoken=fe7JCO4WPYr-fokO_2SwH0iX; _yuque_session=Yxvt7FKhpiAjwHBHiuoSDAnTd7db5PHZpOgska-y5iI_9w1LaxXkVJLS70u2kPIP-YoXyjboffYRne10BrJJig==; current_theme=default; acw_tc=0b68a81616702064709506907e7158eef12b76c54d44f2b09c8b7780115c3e; lang=zh-cn",
+      cookie,
     },
   });
-  const text = await response.text();
-  const fileName = getFileName(response.headers.get("content-disposition"));
-  const parsedText = par
-  
+  if (response.ok) {
+    const text = await response.text();
+    return parseText(text);
+  }
+  return response.ok;
 };
 
-
-
 /*------ main ------*/
-const contentUrl = await getExportedContentUrl(72844784);
-getExportedContent(contentUrl);
+logger.info("script start");
+const noteMap = await getNoteMap();
+for (let { id, title } of noteMap) {
+  logger.info(`noteId = ${id}`);
+  const contentUrl = await getExportedContentUrl(id);
+  if (!contentUrl) continue;
+  const content = await getExportedContent(contentUrl);
+  if (!content) continue;
+  writeFile(`${title}.md`, content);
+}
+logger.info("script end");
